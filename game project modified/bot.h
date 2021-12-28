623346da2f39425feba85008e23a392c1681d011
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2021-12-28 21:27:14 +0200

    Add files via upload
    bugs fixed
    color update
    2 player updated 
    bot updated
    everything is updated

---
+++ game project modified/bot.h
@@ -0,0 +1,57 @@
+struct playerinfo player1;
+struct playerinfo player2;
+void AI(int player[20][20],int n,int flag){
+    int r=0,c=0;
+        for(int i=0 ;i<n;i++){
+          for(int j=0;j<n;j++){
+             if((i+j)%2==0){
+               if(player[i][j]==0){
+                  if((player[i-1][j]!=0)&&(player[i+1][j]!=0)&&(player[i][j+1]!=0)&&(player[i][j-1]==0)){
+                      player[i][j-1]= 2;
+                      player2.playerscore++;
+                      flag=1;
+                  }
+                  else if((player[i-1][j]!=0)&&(player[i+1][j]!=0)&&(player[i][j+1]==0)&&(player[i][j-1]!=0)){
+                       player[i][j-1]= 2;
+                       player2.playerscore++;
+                       flag=1;
+                  }
+                  else if((player[i-1][j]!=0)&&(player[i+1][j]==0)&&(player[i][j+1]!=0)&&(player[i][j-1]!=0)){
+                         player[i+1][j]= 2;
+                         player2.playerscore++;
+                         flag=1;
+                  }
+                  else if((player[i-1][j]==0)&&(player[i+1][j]!=0)&&(player[i][j+1]!=0)&&(player[i][j-1]!=0)){
+                      player[i-1][j]= 2;
+                      player2.playerscore++;
+                      flag=1;
+                  }
+
+                  else{
+                    goto place2;
+                  }
+               }
+             }
+          }
+        }
+        place2:
+        while(1){
+            srand(time(NULL));
+            r=rand()%11;
+            c=rand()%11;
+            if(((r+c)%2==0)||((r<n)&&(c<n))){
+            }
+            else{
+                if(player[r][c]==0){
+                    if(r%2==0){
+                        player[r][c]=2;
+                            break;
+                    }
+                    else{
+                        player[r][c]=2;
+                            break;
+                    }
+                }
+            }
+        }
+}
