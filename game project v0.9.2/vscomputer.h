6a4c4f42d24106b3008df68614be4763d77f2a0a
AhmedAly01 <96546133+AhmedAly01@users.noreply.github.com>
2022-01-07 21:11:19 +0200

    Add files via upload
    -fixed most bugs.
    -added extra AI (smartness).

---
+++ game project v0.9.2/vscomputer.h
@@ -0,0 +1,228 @@
+#define BRED "\e[1;31m"
+#define BBLU "\e[1;34m"
+#define reset "\e[0m"
+#define REDB "\e[41m"
+#define BLUB "\e[44m"
+
+void gamemode1(int y,int n,int player[20][20],char playername1[]){
+  int i;
+  int remMoves;
+  int flag;
+  int computerPlayed;
+  int history[60][3];
+  makeArray(n,player);
+  player1.playerscore= 0;
+  player2.playerscore= 0;
+  system("cls");
+  printf("ENTER PLAYER 1 NAME : ");
+  scanf (" %[^\n]%*c", playername1);
+  int r,c;
+  i=0;
+  time_t start,end1;
+  start=time(NULL);
+  end1=start;
+  if (y==1) remMoves=12;
+  else remMoves=60;
+  place1:
+  startOfGame:
+  while(1){
+        system("cls");
+            printArray(n,player);
+            flag=0;
+            computerPlayed=0;
+             if(i%2==0){
+                printf (BRED"\n\nPlayer 1 turn\n\n"reset);
+                printf (BRED"remaining moves: %d \n"reset, remMoves);
+                printf ("player score = %d\t\tComputer score = %d\n\n", player1.playerscore, player2.playerscore);
+                printf ("\t\tTime passed= %.2lf\n",difftime(end1,start));
+                printf ("\nEnter (1,1) for undo\t(2,2) for redo\t(3,3) for save\n\n");
+                printf(BRED"Enter number of row: "reset);
+                scanf(" %d",&r);
+                printf(BRED"Enter number of column: "reset);
+                scanf (" %d",&c);
+                if(r==1&&c==1){
+                    undo(history,&i,player,n);
+                    goto place1;
+                }
+                else if(r==2 && c==2){
+                    redo(history,&i,player,n);
+                    goto place1;
+                }
+                else if (r==3 && c==3) {
+                    system ("cls");
+                    printf ("Save files:\n\n1.save file 1.\n2.save file 2.\n3.save file 3.\n\n");
+                    printf ("Enter save file number: ");
+                    int fileNum;
+                    scanf ("%d", &fileNum);
+                    save(player,n,2,i,remMoves,start,end1,player1.playerscore,player2.playerscore,playername1,"computer",fileNum);
+                    printf ("\nSuccessfully save to file %d\n", fileNum);
+                    system ("pause");
+                    goto startOfGame;
+                }
+                else if((r+c)%2==0){
+                      printf("invalid move\n");
+                      system("pause");
+                      goto place1;
+                }
+                else if (r>=n&&c>=n){
+                    printf("invalid move\n");
+                      system("pause");
+                      goto place1;
+                }
+                else{
+                      if(player[r][c]==0){
+                        player[r][c]=1;
+                        history[i][0]=i;
+                        history[i][1]=r;
+                        history[i][2]=c;
+
+                      }
+                      else{
+                        printf("invalid move\n");
+                        system("pause");
+                        goto place1;
+                      }
+                    }
+                }
+             else{
+                for (int j=1;j<=(n-2);j+=2) {
+                    for (int k=1;k<=(n-2);k+=2) {
+                        if (!computerPlayed) {
+                            if ((player[j-1][k]==0)&&(player[j+1][k]!=0)&&(player[j][k-1]!=0)&&(player[j][k+1]!=0)&&(player[j][k]==0)) {
+                                player[j-1][k]=2;
+                                history[i][0]=i;
+                                history[i][1]=j-1;
+                                history[i][2]=k;
+                                computerPlayed=1;
+                            }
+                            if ((player[j-1][k]!=0)&&(player[j+1][k]==0)&&(player[j][k-1]!=0)&&(player[j][k+1]!=0)&&(player[j][k]==0)) {
+                                player[j+1][k]=2;
+                                history[i][0]=i;
+                                history[i][1]=j+1;
+                                history[i][2]=k;
+                                computerPlayed=1;
+                            }
+                            if ((player[j-1][k]!=0)&&(player[j+1][k]!=0)&&(player[j][k-1]==0)&&(player[j][k+1]!=0)&&(player[j][k]==0)) {
+                                player[j][k-1]=2;
+                                history[i][0]=i;
+                                history[i][1]=j;
+                                history[i][2]=k-1;
+                                computerPlayed=1;
+                            }
+                            if ((player[j-1][k]!=0)&&(player[j+1][k]!=0)&&(player[j][k-1]!=0)&&(player[j][k+1]==0)&&(player[j][k]==0)) {
+                                player[j][k+1]=2;
+                                history[i][0]=i;
+                                history[i][1]=j;
+                                history[i][2]=k+1;
+                                computerPlayed=1;
+                            }
+                        }
+                    }
+                }
+                for (int j=1;j<=(n-2);j+=2) {
+                    for (int k=1;k<=(n-2);k+=2) {
+                        if (!computerPlayed) {
+                            if ((player[j-1][k]==0)&&(player[j+1][k]==0)&&(player[j][k-1]==0)&&(player[j][k+1]!=0)&&(player[j][k]==0)) {
+                                player[j-1][k]=2;
+                                history[i][0]=i;
+                                history[i][1]=j-1;
+                                history[i][2]=k;
+                                computerPlayed=1;
+                            }
+                            if ((player[j-1][k]==0)&&(player[j+1][k]==0)&&(player[j][k-1]!=0)&&(player[j][k+1]==0)&&(player[j][k]==0)) {
+                                player[j-1][k]=2;
+                                history[i][0]=i;
+                                history[i][1]=j-1;
+                                history[i][2]=k;
+                                computerPlayed=1;
+                            }
+                            if ((player[j-1][k]==0)&&(player[j+1][k]!=0)&&(player[j][k-1]==0)&&(player[j][k+1]==0)&&(player[j][k]==0)) {
+                                player[j-1][k]=2;
+                                history[i][0]=i;
+                                history[i][1]=j-1;
+                                history[i][2]=k;
+                                computerPlayed=1;
+                            }
+                            if ((player[j-1][k]!=0)&&(player[j+1][k]==0)&&(player[j][k-1]==0)&&(player[j][k+1]==0)&&(player[j][k]==0)) {
+                                player[j][k+1]=2;
+                                history[i][0]=i;
+                                history[i][1]=j;
+                                history[i][2]=k+1;
+                                computerPlayed=1;
+                            }
+                        }
+                    }
+                }
+                for (int j=1;j<=(n-2);j+=2) {
+                    for (int k=1;k<=(n-2);k+=2) {
+                        if (!computerPlayed) {
+                            if ((player[j-1][k]==0)&&(player[j+1][k]==0)&&(player[j][k-1]==0)&&(player[j][k+1]==0)&&(player[j][k]==0)) {
+                                player[j-1][k]=2;
+                                history[i][0]=i;
+                                history[i][1]=j-1;
+                                history[i][2]=k;
+                                computerPlayed=1;
+                            }
+                        }
+                    }
+                }
+                for (int j=0; j<n; j++) {
+                    for (int k=0; k<n; k++) {
+                        if (!computerPlayed) {
+                            if ((j+k)%2!=0) {
+                                if ((j%2==0)||(k%2==0)) {
+                                    if (player[j][k]==0) {
+                                        player[j][k]=2;
+                                        history[i][0]=i;
+                                        history[i][1]=j;
+                                        history[i][2]=k;
+                                        computerPlayed=1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        for (int j=1;j<=(n-2);j+=2) {
+         for (int k=1;k<=(n-2);k+=2) {
+            if ((player[j-1][k]!=0)&&(player[j+1][k]!=0)&&(player[j][k-1]!=0)&&(player[j][k+1]!=0)&&(player[j][k]==0)) {
+                if (i%2==0){
+                   player[j][k]=3;
+                   player1.playerscore++;
+                   flag=1;
+                }
+                else{
+                     player[j][k]=4;
+                     player2.playerscore++;
+                     flag=1;
+                }
+            }
+         }
+       }
+           printArray(n, player);
+           end1=time(NULL);
+           if(flag==0){
+            i++;
+           }
+
+           remMoves--;
+           if(remMoves==0){
+                system("cls");
+                printArray(n,player);
+                if (player1.playerscore>player2.playerscore) {
+                    printf (BRED"\n\nPlayer 1 wins!!\n\n"reset);
+                    rankHandle(playername1,player1.playerscore);
+                }
+                else if (player1.playerscore<player2.playerscore) {
+                    printf (BBLU"\n\nComputer wins!!\n\n"reset);
+                }
+                else {
+                    printf ("\n\nDraw!\n\n");
+                }
+                printf ("\nPress any key to go to main menu\n");
+                system ("pause");
+            break;
+           }
+      }
+}
